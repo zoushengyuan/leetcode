29d769a9d80d918298a35ef702eb77b1f96349e8
zoushengyuan <ctczsy@sina.com>
2021-02-23 09:40:50 +0800

    s0023

--- s0023/s0023.go
+++ s0023/s0023.go
@@ -2,50 +2,47 @@ package main
 
 import (
 	"fmt"
+	. "leetcode/linkedlist"
 )
 
-/*
-func findMin(lists []*ListNode) int {
-	n := len(lists)
-	if n == 0 {
-		return -1
-	}
-	minVal := math.MaxInt64
-	minPos := -1
-	//var list *ListNode
-	for i, list := range lists {
-		if list != nil && list.Val < minVal {
-			minVal = list.Val
-			minPos = i
-		}
-	}
-	return minPos
-}
-
-func mergeKLists(lists []*ListNode) *ListNode {
-	head := &ListNode{}
-	p := head
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := ListNode{}
+	current := &head
 	for {
-		pos := findMin(lists)
-		if pos == -1 {
-			p.Next = nil
+		if l1 == nil {
+			current.Next = l2
 			break
+		} else if l2 == nil {
+			current.Next = l1
+			break
+		} else if l1.Val > l2.Val {
+			current.Next = l2
+			current = l2
+			l2 = l2.Next
+		} else {
+			current.Next = l1
+			current = l1
+			l1 = l1.Next
 		}
-		p.Next = lists[pos]
-		p = p.Next
-		lists[pos] = lists[pos].Next
-		//fmt.Println(p.Val)
 	}
 	return head.Next
 }
-*/
 
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	l1 := lists[0]
+	for _, l2 := range lists[1:] {
+		l1 = mergeTwoLists(l1, l2)
+	}
+	return l1
+}
 
 func main() {
-	list1 := createList([]int{1, 4, 5})
-	list2 := createList([]int{1, 3, 4})
-	list3 := createList([]int{2, 6})
+	list1 := CreateList([]int{1, 4, 5})
+	list2 := CreateList([]int{1, 3, 4})
+	list3 := CreateList([]int{2, 6})
 	lists := []*ListNode{list1, list2, list3}
-	//fmt.Println(findMin(lists))
-	fmt.Println(traverseList(mergeKLists(lists)))
+	fmt.Println(TraverseList(mergeKLists(lists)))
 }
